6ae79736bf8e6365a1e7ed05936e0026b657708a
Joel De Conto <joel_d.c@hotmail.com>
2023-01-12 18:36:21 -0300

    Update generatePdf.js

--- generatePdf.js
+++ generatePdf.js
@@ -36,7 +36,7 @@ async function sendEmail(emailAddr, rPath, filename, emailBodyPath, result){
       });
 
       //console.log('Verificando servidor de emails...');
-        console.log(`Tentativa de email...  ${process.eventNames.MAIL_USER}`);
+        console.log(`Tentativa de email...  ${process.env.MAIL_USER}`);
 
     // verify connection configuration
         //transporter.verify(function (error, success) {
